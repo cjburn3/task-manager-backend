73826d71796ca06dd510aa738bf14badd31e9a41
cjburn3 <156855354+cjburn3@users.noreply.github.com>
2024-10-12 19:53:00 -0400

    updates

--- dist/supabaseClient.js
+++ dist/supabaseClient.js
@@ -1,8 +1,15 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.supabase = void 0;
-require("dotenv/config");
 const supabase_js_1 = require("@supabase/supabase-js");
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
-exports.supabase = (0, supabase_js_1.createClient)(supabaseUrl, supabaseAnonKey);
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const supabaseUrl = process.env.SUPABASE_URL || '';
+const supabaseKey = process.env.SUPABASE_KEY || '';
+if (!supabaseUrl || !supabaseKey) {
+    throw new Error('Supabase URL or Key is missing.');
+}
+exports.supabase = (0, supabase_js_1.createClient)(supabaseUrl, supabaseKey);
